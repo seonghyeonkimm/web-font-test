ee450d26630f102be6d172dc69a3c73ed1ad822d
demian <demian@kcd.co.kr>
2021-04-01 17:34:45 +0900

    add font

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { ReactComponent as Arrow } from './arrow-right.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="m">
+        <p>KCD font</p>
+        <div className="numeric kcd">
+          1,234,567,890
+        </div>
+      </div>
+      <div className="m">
+        <p>Noto</p>
+        <div className="numeric noto">
+          1,234,567,890
+        </div>
+      </div>
+      <div className="m">
+        <p>Spoqa</p>
+        <div className="numeric spoqa">
+          1,234,567,890
+        </div>
+      </div>
+      <div className="m">
+        <p>KCD font</p>
+        <button className="button kcd">
+          <span className="label">
+            버튼 레이블
+          </span>
+          <span className="icon">
+            <Arrow />
+          </span>
+        </button>
+      </div>
+      <div className="m">
+        <p>noto sans</p>
+        <button className="button noto">
+          <span className="label">
+            버튼 레이블
+          </span>
+          <span className="icon">
+            <Arrow />
+          </span>
+        </button>
+      </div>
+      <div className="m">
+        <p>Spoqa Han Sans</p>
+        <button className="button spoqa">
+          <span className="label">
+            버튼 레이블
+          </span>
+          <span className="icon">
+            <Arrow />
+          </span>
+        </button>
+      </div>
     </div>
   );
 }
